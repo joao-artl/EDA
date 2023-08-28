bfb1d268a8922bdda77652070adeadcbb380edbb
Joao Artur Leles <jalxpinheiro@gmail.com>
2023-08-28 02:54:45 -0300

    Adicionando exercicios thums e zerinho_ou_um

--- Lista1NaoOficial/thums.c
+++ Lista1NaoOficial/thums.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    
+    int batidas;
+    scanf("%d", &batidas);
+    for(int i = 1; i <= batidas; i++){
+        if((i % 2) == 0){
+            printf("THUMS THUMS THUMS THUMS THUMS THUMS\n");
+        }
+        else{
+            printf("THUMS THUMS THUMS\n");
+        }
+    }
+    return 0;
 }
--- Lista1NaoOficial/zerinho_ou_um.c
+++ Lista1NaoOficial/zerinho_ou_um.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    
+    int A,B,C;
+    scanf("%d %d %d", &A, &B, &C);
+    if(A != B && B == C){
+        printf("A\n");
+    }
+    if(B != A && A == C){
+        printf("B\n");
+    }
+    if(C != B && B == A){
+        printf("C\n");
+    }
+        if(A == B && B == C && A == C){
+        printf("empate\n");
+    }
 }
