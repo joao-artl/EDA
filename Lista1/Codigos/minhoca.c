cae4a3de74a8f9bcc8301506ffc1307cca7cf027
Joao Artur Leles <jalxpinheiro@gmail.com>
2023-09-18 10:24:28 -0300

    adicionando codigo minhoca

--- Lista1/Codigos/minhoca.c
+++ Lista1/Codigos/minhoca.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
-int main(){
-    int linhas, colunas,caminho = 0, maior = 0;
+int main() {
+    int linhas, colunas, caminho = 0, maior = 0;
     scanf("%d %d", &linhas, &colunas);
     int campo[linhas][colunas];
-    int i = 0, j = 0;
-    for(i = 0; i < linhas; i++){
-        for(j = 0; j < colunas; j++){
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
             scanf("%d", &campo[i][j]);
         }
     }
-    for(i = 0; i < linhas; i++){
-        for(j = 0; j < colunas; j++){
-            caminho = caminho + campo[i][j];
+
+    for (int i = 0; i < colunas; i++) {
+        for (int j = 0; j < linhas; j++) {
+            caminho += campo[j][i];
         }
-    }
-    for(i = 0; i < linhas; i++){
-        for(j = 0; j < colunas; j++){
-            scanf("%d", &campo[i][j]);
+        if (maior < caminho) {
+            maior = caminho;
         }
+        caminho = 0;
     }
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            caminho += campo[i][j];
+        }
+        if (maior < caminho) {
+            maior = caminho;
+        }
+        caminho = 0;
+    }
+    printf("%d\n", maior);
+    return 0;
 }
