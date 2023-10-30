b24beb28f1a0945593c19bcd555a9c7e3145936c
Joao Artur Leles <jalxpinheiro@gmail.com>
2023-10-29 23:00:35 -0300

    fim lista 2

--- Lista2/Codigos/remocao.c
+++ Lista2/Codigos/remocao.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct celula {
-int dado;
-struct celula *prox;
+	int dado;
+	struct celula *prox;
 } celula;
 
-int remove_depois (celula *p){
-    celula *lista =
-}
-
-void remove_elemento (celula *le, int x){
-    celula *p = le->prox;
-
-    p->prox = p->prox->prox;
-    return;
-}
-
-void remove_todos_elementos (celula *le, int x){
-    celula *p = le->prox;
-    while (p != NULL){
-        
+celula * busca_lista_posicao(celula * le, int x) {
+  celula * elem = le;
+  for (elem = elem; elem -> prox != NULL; elem = elem -> prox) {
+    if (elem -> prox -> dado == x) {
+      return elem;
     }
-    return;
+  }
+  return elem;
+}
+
+
+int remove_depois(celula *p){
+  if (p->prox!=NULL)
+  {
+    celula *prox_p = p->prox;
+    int dado_removido = prox_p->dado;
+    p->prox = prox_p->prox;
+    free(prox_p);
+    return dado_removido;
+  } else {
+     return 1;
+  }
+ 
+}
+
+void remove_elemento(celula *le, int x){
+  //Procurando o elemento x em le
+  if(le->prox!=NULL){
+    celula *antes_x = busca_lista_posicao(le, x);
+    celula *x_encontrado = antes_x->prox;
+
+    antes_x->prox = x_encontrado->prox;
+
+    free(x_encontrado);
+  }
+  
+}
+
+void remove_todos_elementos(celula *le, int x){
+  celula *anterior = le;
+  if(le->prox!=NULL){
+    for (celula *elem = le->prox; elem != NULL; elem = elem->prox){
+      if(elem->dado == x){
+        anterior->prox = elem->prox;
+      }
+      anterior = elem;
+    }
+  }
 }
