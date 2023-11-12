f673d769c6707049479d70ee0368df03821f07a6
Joao Artur Leles <jalxpinheiro@gmail.com>
2023-11-12 17:44:08 -0300

    Adicionando questão

---
+++ Lista3/Codigos/enfileiraListaEstatica.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct fila {
+    int *dados;
+    int N, p, u;
+} fila;
+
+int enfileira(fila *f, int x) {
+    if(f->u == f->N) {
+        f->N *= 2;
+        int *novo = realloc(f->dados ,f->N*sizeof(int));
+        if(novo == NULL) return 0;
+        f->dados = novo;
+    } 
+        
+    f->dados[f->u++] = x;
+    return 1;
+}
