2be47e069331656f0c8e19a3d8c93b980c84a24c
Joao Artur Leles <jalxpinheiro@gmail.com>
2023-11-16 01:19:19 -0300

    começando busca binária

---
+++ Lista4/Codigos/buscaBinária.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int buscaBinaria(){
+
+}
+
+int main(){
+    int ordenados, procurados;
+    scanf("%d %d", &ordenados, &procurados);
+    int vetor[ordenados], numeros;
+    for(int i = 0; i < ordenados; i++){
+        scanf("%d", &vetor[i]);
+    }
+    for(int j = 0; j < procurados; j++){
+        scanf("%d", &numeros);
+
+ 
+
+ 
+    }
+    return 0;
+}
